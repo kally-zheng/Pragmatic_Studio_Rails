28df1dbbc3f5877237cf812a8595193b5a783c53
Kally-Zheng <kally.zheng@appfolio.com>
2017-06-26 11:38:18 -0700

    Lesson 15: TDD

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -34,5 +34,12 @@ movie3 = Movie.create(title: "Spider-Man",
   expect(page).to have_text(movie2.title)
   expect(page).to have_text(movie3.title)
 end
+it "does not show a movie that hasn't yet been released" do
+  movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+  visit movies_path
+
+  expect(page).not_to have_text(movie.title)
+end
   
 end
